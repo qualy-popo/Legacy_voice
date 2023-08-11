ec746f46f45ae1385c8efa9a1ec76ce7369e5c5a
SuPoPoZaw <supopozaw@qualymm.com>
2023-08-11 14:55:05 +0630

    detail page

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,24 +12,25 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
+<!-- <footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'legacy-voice' ) ); ?>">
+			<a href="<?php echo esc_url(__('https://wordpress.org/', 'legacy-voice')); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'legacy-voice' ), 'WordPress' );
+				printf(esc_html__('Proudly powered by %s', 'legacy-voice'), 'WordPress');
 				?>
 			</a>
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'legacy-voice' ), 'legacy-voice', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf(esc_html__('Theme: %1$s by %2$s.', 'legacy-voice'), 'legacy-voice', '<a href="http://underscores.me/">Underscores.me</a>');
 				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+		</div>
+	</footer> -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
--- single-voice.php
+++ single-voice.php
@@ -15,200 +15,35 @@ get_header('1'); ?>
 <main class="voice_page_detail">
     <section>
         <div class="voice_container voice_detail">
-            <div class="m-w920">
-                <div class="voice_detail_bg">
-                    <div class="voice_detail_flex">
-                        <div class="voice_detail_card">
-                            <?php
-                            $male_profile_images = [
-                                "/img/voice/profile_icon/male_34_img.png",
-                                "/img/voice/profile_icon/male_56_img.png",
-                                "/img/voice/profile_icon/male_78_img.png",
-                                "/img/voice/profile_icon/male_910_img.png"
-                            ];
-
-                            $female_profile_images = [
-                                "/img/voice/profile_icon/female_34_img.png",
-                                "/img/voice/profile_icon/female_56_img.png",
-                                "/img/voice/profile_icon/female_78_img.png",
-                                "/img/voice/profile_icon/female_910_img.png"
-                            ];
-
-                            $default_img = "/img/voice/profile_icon/default_img.png";
-
-                            $voice_add = SCF::get("voice_add");
-                            $voice_age = SCF::get("voice_age");
-                            $voice_gender = SCF::get("voice_gender");
-
-                            $trim_age = rtrim($voice_age, "代");
-                            $int_age = (int) $trim_age;
-
-
-                            switch ($int_age) {
-                                case $int_age == 30 || $int_age == 40:
-                                    if ($voice_gender == "男性") {
-                                        $img = $male_profile_images[0];
-                                    } else if ($voice_gender == "女性") {
-                                        $img = $female_profile_images[0];
-                                    } else {
-                                        $img = $default_img;
-                                    }
-                                    break;
-                                case $int_age == 50 || $int_age == 60:
-                                    if ($voice_gender == "男性") {
-                                        $img = $male_profile_images[1];
-                                    } else if ($voice_gender == "女性") {
-                                        $img = $female_profile_images[1];
-                                    } else {
-                                        $img = $default_img;
-                                    }
-                                    break;
-                                case $int_age == 70 || $int_age == 80:
-                                    if ($voice_gender == "男性") {
-                                        $img = $male_profile_images[2];
-                                    } else if ($voice_gender == "女性") {
-                                        $img = $female_profile_images[2];
-                                    } else {
-                                        $img = $default_img;
-                                    }
-                                    break;
-                                case $int_age == 90 || $int_age == 100:
-                                    if ($voice_gender == "男性") {
-                                        $img = $male_profile_images[3];
-                                    } else if ($voice_gender == "女性") {
-                                        $img = $female_profile_images[3];
-                                    } else {
-                                        $img = $default_img;
-                                    }
-                                    break;
-                                default:
-                                    $img = $default_img;
-                                    break;
-                            }
-
-                            $voice_service = SCF::get("voice_service");
-
-                            $service_imgs = [
-                                "/img/voice/service_icon/omakase_img.png",
-                                "/img/voice/service_icon/tax_return_img.png",
-                                "/img/voice/service_icon/tax_diagnosis_img.png",
-                            ];
-
-                            ?>
-                            <div class="voice_detail_head <?php echo (empty($voice_service)) ? "one_fr" : ""; ?>">
-                                <div class="profile_block">
-                                    <div class="profile_picture">
-                                        <img src="<?php echo bloginfo('template_url');
-                                                    echo $img; ?>" alt="<?php echo $voice_gender; ?>" />
-                                    </div>
-                                    <?php if (!empty($voice_add)) { ?>
-                                        <p class="address"><?php echo ($voice_add); ?></p>
-                                    <?php } ?>
-                                    <p class="apperance"><?php if ($int_age > 0) { ?><span class="age"><?php echo ($int_age); ?>代</span><?php } ?><span class="gender"><?php echo ($voice_gender); ?></span></p>
+            <div class="m-w920 mt50">
+                <div class="voice_head_txt">
+                    <p>川崎市 / 30代 / 女性</p>
+                </div>
+                <div class="voice_detail_bg ">
+                    <div class="voice_detail_flex_sec">
+                        <p class="upper_txt mt20 mb20">利用サービス </p>
+                        <div class="detail_flex_list detail_flex">
+                            <div class="list01">
+                                <div class="txt_img">
+                                    <img src="<?php echo bloginfo('template_url'); ?>/assets/img/voice/detail_img_01.png" />
                                 </div>
-                                <?php if (!empty($voice_service)) {
-
-                                    $service_string = implode($voice_service);
-                                    // echo($service_string);
-
-                                    switch ($service_string) {
-                                        case $service_string == "おまかせパック":
-                                            $service_img = $service_imgs[0];
-                                            break;
-                                        case $service_string == "相続税申告":
-                                            $service_img = $service_imgs[1];
-                                            break;
-                                        case $service_string == "【相続税診断付き】公正証書遺言作成":
-                                            $service_img = $service_imgs[2];
-                                            break;
-                                        default:
-                                            $service_img = $service_imgs[0];
-                                    }
-
-                                    // echo($service_img);
-                                ?>
-                                    <div class="service_block">
-                                        <div class="service_picture">
-                                            <img src="<?php echo bloginfo('template_url');
-                                                        echo $service_img; ?>" alt="<?php echo $service_string; ?>" />
-                                        </div>
-                                        <p class="service_used">【利用サービス】</p>
-
-                                        <?php foreach ($voice_service as $service) { ?>
-                                            <p class="service_name"><?php echo esc_html($service); ?></p>
-                                        <?php } ?>
-                                    </div>
-                                <?php } ?>
+                                <p class="txt_content">相続税申告</p>
                             </div>
-                            <div class="card_body">
-                                <?php
-                                $scf_evaluation = SCF::get("voice_evaluation");
-                                if ($scf_evaluation !== "非表示") {
-                                    $trim_evaluation = trim($scf_evaluation, "点");
-                                    $float_evaluation = (float) $trim_evaluation;
-                                    if ($float_evaluation >= 0) {
-                                ?>
-                                        <div class="evaluation_block">
-                                            <div class="evaluation_txt">
-                                                <p>評価</p>
-                                            </div>
-                                            <div class="rating">
-                                                <div class="stars">
-                                                    <?php
-                                                    $rating = $float_evaluation;
-                                                    for ($i = 1; $i <= 5; $i++) {
-                                                        $class = '';
-                                                        if ($i <= $rating) {
-                                                            $class = 'rated';
-                                                        }
-                                                        if ($i == ceil($rating) && $rating - floor($rating) == 0.5) {
-                                                            $class = 'half-rated';
-                                                        }
-                                                    ?>
-                                                        <span class="star <?php echo $class; ?>"></span>
-                                                    <?php } ?>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    <?php
-                                    }
-                                    ?>
-                                <?php
-                                }
-                                ?>
-                                <div class="reason_block">
-                                    <?php $voice_reason = SCF::get('voice_reason'); ?>
-                                    <?php if (!empty($voice_reason)) : ?>
-                                        <div class="reason_txt">
-                                            <p>選んだ理由</p>
-                                        </div>
-                                    <?php endif ?>
-                                    <div class="reason_desc">
-                                        <?php if (!empty($voice_reason)) : ?>
-                                            <?php foreach ($voice_reason as $reason) : ?>
-                                                <div class="desc_txt"><?php echo esc_html($reason); ?></div>
-                                            <?php endforeach; ?>
-                                        <?php endif; ?>
-                                    </div>
+                            <div class="list02">
+                                <div class="txt_img">
+                                    <img src="<?php echo bloginfo('template_url'); ?>/assets/img/voice/detail_img_02.png" />
                                 </div>
-                                <div class="good_point_block">
-                                    <?php $voice_point = SCF::get('voice_point'); ?>
-                                    <?php if (!empty($voice_point)) : ?>
-                                        <div class="god_point_txt">
-                                            <p>よかった点</p>
-                                        </div>
-                                    <?php endif ?>
-                                    <div class="good_points">
-                                        <?php if (!empty($voice_point)) : ?>
-                                            <?php foreach ($voice_point as $point) : ?>
-                                                <div class="good_point"><?php echo esc_html($point); ?></div>
-                                            <?php endforeach; ?>
-                                        <?php endif; ?>
-                                    </div>
+                                <p class="txt_content">不動産投資コンサルティング</p>
+                            </div>
+                            <div class="list03">
+                                <div class="txt_img">
+                                    <img src="<?php echo bloginfo('template_url'); ?>/assets/img/voice/detail_img_03.png" />
                                 </div>
+                                <p class="txt_content">お助け税務調査</p>
                             </div>
                         </div>
                     </div>
+
                     <?php
                     $voice_comment = SCF::get('voice_comment');
                     if (!empty($voice_comment)) { ?>
