e0e4c6374be94c26202420c9d63bdbab11a89fba
WinWinHtetKyawQualy <winwinhtetkyaw@qualymm.com>
2023-08-11 14:34:05 +0630

    adding matchHeight js code

---
+++ assets/js/common2023.js
@@ -0,0 +1,39 @@
+const AccorTitles = Array.from(document.querySelectorAll(".accor_item"));
+
+const AccorContents = Array.from(document.querySelectorAll(".accor_item .accor_content"));
+
+AccorContents.forEach(function (item) {
+  item.addEventListener("click", function (e) {
+    e.stopPropagation();
+  });
+});
+
+const removeClassFromOthers = (array, indx) => {
+  array.forEach(function (item, index) {
+    if (indx !== index) item.classList.remove("active_item");
+  });
+};
+
+AccorTitles.forEach(function (title, indx) {
+  title.addEventListener("click", function () {
+    removeClassFromOthers(AccorTitles, indx);
+    this.classList.toggle("active_item");
+  });
+});
+
+//Voice Detail Image Modal Box
+$(".voice_bg_img").click(function () {
+  $("#modal").html($(this).prop("outerHTML"));
+  $("#modal").fadeIn(200);
+  return false;
+});
+
+$("#modal").click(function () {
+  $("#modal").fadeOut(200);
+  return false;
+});
+
+
+// ----- Voice match-height ------
+$(".voice_card .reason_block").matchHeight();
+$(".voice_card .good_point_block").matchHeight();
